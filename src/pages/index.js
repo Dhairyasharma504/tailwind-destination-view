63a58808ffd59a11a23bdc959a08e2e49da48afd
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-01-25 19:00:05 +0530

    side-navbar add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Hero from '../components/Hero';
+import Navbar from '../components/Navbar';
 
 const index = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
