1087e6421c611ee798435593a2b81b6f8c9bfd7e
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-01-27 14:33:30 +0530

    g

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="w-full h-[80vh] bg-images top-0 left-0 bg-cover bg-fixed">
       <div className="bg-black/20 absolute top-0 left-0 w-full h-[80vh] " />
       <div className=" max-w-screen-xl h-full flex flex-col justify-center text-white">
-        <p className=" md:pl-36 pl-4   font-secondary font-normal text-brand">
+        <p className=" md:pl-36 pl-4   font-secondary font-normal text-red-500">
           <span className="md:text-8xl text-5xl"> Find your </span>{' '}
           <span className="md:text-9xl text-7xl">Next tour!</span>
         </p>
