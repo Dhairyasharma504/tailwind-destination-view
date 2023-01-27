005971d7a4e334a1b207506f282c56ebe886dd29
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-01-27 14:35:19 +0530

    y

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className=" max-w-screen-xl h-full flex flex-col justify-center text-white">
         <p className=" md:pl-36 pl-4   font-secondary font-normal text-red-500">
           <span className="md:text-8xl text-5xl"> Find your </span>{' '}
-          <span className="md:text-9xl text-7xl">Next tour!</span>
+          <span className="md:text-9xl text-6xl">Next tour!</span>
         </p>
       </div>
     </div>
