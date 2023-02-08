317de11a6c0287af8b55d5eb81519c36c5e0b530
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-02-08 15:41:50 +0530

    fix secreen size hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,17 +5,17 @@ const Hero = () => {
   return (
     <div className="w-full h-[90vh] bg-images top-0 left-0 bg-cover  ">
       <div className="bg-black/25 h-[90vh]">
-        <div className=" p-6  h-full flex flex-col justify-center  w-full text-lightshade ">
+        <div className=" p-6  h-full flex flex-col justify-center items-start  w-full text-lightshade ">
           {/* <p className=" md:pl-36 pl-4   font-secondary font-normal text-brand">
           <span className="md:text-8xl text-5xl"> Find your </span>{' '}
           <span className="md:text-9xl text-6xl">Next tour!</span>
         </p> */}
 
-          <div class="   ">
-            <p class="text-xl sm:text-2xl xl:text-5xl text-center md:text-left  font-extrabold  leading-6 xl:leading-10 text-lightshade pb-6">
+          <div className=" bg-red-200 md:bg-blue-500 md:w-3/4 lg:bg-yellow-400 xl:w-1/2 2xl:bg-green-200">
+            <p className="text-xl sm:text-2xl xl:text-5xl text-center md:text-left  font-extrabold  leading-6 xl:leading-10 text-lightshade pb-6">
               Let&apos;s Enjoy The Wonders of Nature
             </p>
-            <p class="mt-4  text-center md:text-left text-xl leading-normal text-lightshade">
+            <p className="mt-4  text-center md:text-left text-xl leading-normal text-lightshade">
               Looking to create a greater impact with your commercial Christmas
               display? Take a look at the services we have on offer...
             </p>
--- src/components/Subscrib.js
+++ src/components/Subscrib.js
@@ -3,33 +3,33 @@ import Image from 'next/image';
 const Subscrib = () => {
   return (
     <div>
-      <div class="xl:flex items-center 2xl:mx-auto 2xl:container py-16 sm:mx-20">
-        <div class="lg:p-24 sm:p-9 md:p-16 p-4 2xl:w-8/12 xl:w-11/12 w-full sm:border-2 sm:border-gray-800">
-          <h1 class="text-4xl font-semibold leading-9 text-gray-800 ">
+      <div className="xl:flex items-center 2xl:mx-auto 2xl:container py-16 sm:mx-20">
+        <div className="lg:p-24 sm:p-9 md:p-16 p-4 2xl:w-8/12 xl:w-11/12 w-full sm:border-2 sm:border-primary">
+          <h1 className="text-4xl font-semibold leading-9  text-darkshade ">
             Subscribe
           </h1>
-          <p class="text-base leading-4 mt-2 text-gray-600 ">
+          <p className="text-base leading-4 mt-2 text-primary ">
             Subscribe to our newsletter to get news about latest trends
           </p>
-          <div class="sm:flex items-center mt-10 xl:pb-0 pb-32">
-            <div class="flex items-center bg-gray-100 lg:w-96 w-full">
-              <div class="px-4 py-4">
+          <div className="sm:flex items-center mt-10 xl:pb-0 pb-32">
+            <div className="flex items-center bg-gray-100 lg:w-96 w-full">
+              <div className="px-4 py-4">
                 <img
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/newsletter-IV-svg1.svg"
                   alt="email-icon"
                 />
               </div>
               <input
-                class="pl-2 w-full text-base font-medium leading-none text-gray-600 placeholder-gray-600 focus:outline-none bg-gray-100"
+                className="pl-2 w-full text-base font-medium leading-none text-gray-600 placeholder-gray-600 focus:outline-none bg-gray-100"
                 placeholder="Enter your email address here"
               />
             </div>
-            <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 hover:bg-black text-base font-medium leading-none text-white py-4 px-6 bg-gray-800 sm:ml-2 sm:w-auto w-full sm:mt-0 mt-4  ">
+            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 hover:bg-black text-base font-medium leading-none text-white py-4 px-6 bg-gray-800 sm:ml-2 sm:w-auto w-full sm:mt-0 mt-4  ">
               Subscribe
             </button>
           </div>
         </div>
-        <div class="w-1/2 xl:block absolute hidden right-0 2xl:-mr-0 -mr-24">
+        <div className="w-1/2 xl:block absolute hidden right-0 2xl:-mr-0 -mr-24">
           <Image
             src="/images/traveler.jpg.webp"
             alt="Picture of the author"
@@ -37,7 +37,7 @@ const Subscrib = () => {
             height={200}
           />
         </div>
-        <div class="w-full xl:hidden flex items-center justify-center xl:-mt-0 -mt-32 sm:px-0 px-4">
+        <div className="w-full xl:hidden flex items-center justify-center xl:-mt-0 -mt-32 sm:px-0 px-4">
           <Image
             src="/images/traveler.jpg.webp"
             alt="Picture of the author"
