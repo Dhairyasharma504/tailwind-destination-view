20b93ccc7335ae1041542c11e5d3e30e0c19f684
dhairyasharma504 <dhairyasharma2311@gmail.com>
2023-02-11 20:48:29 +0530

    fix

--- src/components/homeSection/HomeHero.js
+++ src/components/homeSection/HomeHero.js
@@ -15,11 +15,6 @@ const data = [
 const SliderItem = ({ item }) => (
   <HeroWrapper bgImage={item.bgImage}>
     <div className=" p-6  h-full flex flex-col justify-center items-start  w-full text-lightshade ">
-      {/* <p className=" md:pl-36 pl-4   font-secondary font-normal text-brand">
-      <span className="md:text-8xl text-5xl"> Find your </span>{' '}
-      <span className="md:text-9xl text-6xl">Next tour!</span>
-    </p> */}
-
       <div className="   md:w-3/4  xl:w-1/2 ">
         <p
           className="text-xl sm:text-2xl xl:text-5xl text-center md:text-left  font-extrabold  leading-6 xl:leading-10 text-lightshade pb-6"
